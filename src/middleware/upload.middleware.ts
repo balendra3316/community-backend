a256d62d8389b3465ce67f5b8bc67f325a9a46e3
soulskool <balendra@soulskool.in>
2025-06-01 18:42:52 +0530

    Remove .env file from version contr

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -22,7 +22,7 @@ const fileFilter = (req: any, file: Express.Multer.File, cb: multer.FileFilterCa
 
 export const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 10 * 1024 * 1024 }, 
   fileFilter
 });
 
